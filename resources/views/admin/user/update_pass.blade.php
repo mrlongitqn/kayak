0df1202b9458d82d7cb87663870e76e786ef6529
LongND <mrlong.itqn@gmail.com>
2017-08-12 01:17:45 +0700

    fix password input

--- resources/views/admin/user/update_pass.blade.php
+++ resources/views/admin/user/update_pass.blade.php
@@ -20,19 +20,19 @@
                 <div class="form-group">
                     {!! Form::label('old_password','Old password', array('class'=>'col-sm-2 control-label')) !!}
                     <div class="col-sm-10">
-                        {!! Form::text('old_password','' ,array('class'=>'form-control')) !!}
+                        {!! Form::password('old_password' ,array('class'=>'form-control')) !!}
                     </div>
                 </div>
                 <div class="form-group">
                     {!! Form::label('new_pass','New password', array('class'=>'col-sm-2 control-label')) !!}
                     <div class="col-sm-10">
-                        {!! Form::text('new_pass','' ,array('class'=>'form-control')) !!}
+                        {!! Form::password('new_pass' ,array('class'=>'form-control')) !!}
                     </div>
                 </div>
                 <div class="form-group">
                     {!! Form::label('new_pass2','Confirm password', array('class'=>'col-sm-2 control-label')) !!}
                     <div class="col-sm-10">
-                        {!! Form::text('new_pass2','' ,array('class'=>'form-control')) !!}
+                        {!! Form::password('new_pass2' ,array('class'=>'form-control')) !!}
                     </div>
                 </div>
                 <div class="form-group">
